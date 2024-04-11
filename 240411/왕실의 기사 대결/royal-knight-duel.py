d9703b67459a71e7c0399f9712ca8b916b5832ff
김진기 <151550964+Jingik@users.noreply.github.com>
2024-04-11 22:57:36 +0900

    Title: 왕실의 기사 대결, Time: 443ms, Memory: 27MB, Status: Passed - CodeTree

--- 240411/왕실의 기사 대결/royal-knight-duel.py
+++ 240411/왕실의 기사 대결/royal-knight-duel.py
@@ -1,62 +1,122 @@
-import sys
-input = sys.stdin.readline
-
-def solve_chess_knights(L, N, Q, Board, knights_info, commands):
-    direction = {0: (-1, 0), 1: (0, 1), 2: (1, 0), 3: (0, -1)}  # 위: 0, 오른: 1, 아래: 2, 왼: 3
-    
-    # 기사 정보 저장
-    knights = {}
-    for i, (r, c, h, w, k) in enumerate(knights_info):
-        positions = set()
-        for dx in range(h):
-            for dy in range(w):
-                positions.add((r + dx - 1, c + dy - 1))
-        knights[i] = {'positions': positions, 'hp': k}
-
-    # 명령 수행
-    for num, order in commands:
-        num -= 1  # 기사 번호는 0부터 시작하도록 조정
-        if knights[num]['hp'] <= 0:  # 이미 쓰러진 기사는 무시
-            continue
-
-        dx, dy = direction[order]
-        moving_knights = [num]  # 이동할 기사들
-        current_positions = knights[num]['positions']
-        while True:
-            new_positions = {(x + dx, y + dy) for x, y in current_positions}
-            next_knight = None
-
-            # 이동 가능 여부 확인 (다른 기사나 벽 확인)
-            for k, knight in knights.items():
-                if k != num and knight['positions'] & new_positions:
-                    next_knight = k
-                    break
-            if next_knight is None and not all(0 <= nx < L and 0 <= ny < L and Board[nx][ny] != 2 for nx, ny in new_positions):
-                break  # 이동할 수 없으면 멈춤
-
-            current_positions = new_positions
-            if next_knight is not None:
-                moving_knights.append(next_knight)
-                num = next_knight
-            else:
-                # 모든 이동한 기사들의 위치 갱신
-                for knight_num in moving_knights:
-                    knights[knight_num]['positions'] = current_positions
-                break
-
-    # 대미지 계산
-    total_damage = 0
-    for knight in knights.values():
-        damage = sum(Board[x][y] == 1 for x, y in knight['positions'])
-        knight['hp'] = max(knight['hp'] - damage, 0)
-        total_damage += damage
-
-    # 생존한 기사들의 대미지 합산
-    return total_damage
-
+from collections import deque
 
 L, N, Q = map(int, input().split())
-Board = [list(map(int, input().split())) for _ in range(L)]
-knights_info = [list(map(int, input().split())) for _ in range(N)]
-commands = [list(map(int, input().split())) for _ in range(Q)]
-print(solve_chess_knights(L, N, Q, Board, knights_info, commands))
+
+board = [list(map(int, input().split())) for _ in range(L)]
+fighter = [list(map(int, input().split())) for _ in range(N)]
+
+sirs = {i+1:[] for i in range(N)}
+order = [list(map(int, input().split())) for _ in range(Q)]
+def get_xy(arr, temp):
+    r,c,h,w = arr[0], arr[1], arr[2], arr[3]
+    for x in range(r, r+h):
+        for y in range(c, c+w):
+                temp.append([x, y])
+    return temp  
+
+def is_board(x, y):
+    return 0 <= x < L and 0 <= y < L
+
+
+def move_sirs(i, d):
+    # 체스판에서 삭제된 기사 이동 명령
+    if i not in sirs.keys():
+        return []
+    
+    # 현존하는 기사들의 좌표 저장
+    states = {}
+    for key in sirs.keys():
+        temp = []
+        states[key] = get_xy(sirs[key][0], temp)
+    
+    # 이동 명령을 받은 기사
+    q = deque()
+    get_xy(sirs[i][0], q)
+    moved_sir.add(i)
+
+
+    while q:
+        x, y = q.popleft()
+        nx = x + dx[d]
+        ny = y + dy[d]
+
+        # 이동한 곳이 격자 밖인 경우
+        if not is_board(nx, ny):
+            return []
+        
+        for key, value in states.items():
+            # 같이 밀릴 기사가 있고, 아직 밀리지 않은 기사인 경우
+            if [nx, ny] in value and key not in moved_sir:
+                moved_sir.add(key)
+                get_xy(sirs[key][0], q)
+        
+        # 밀린 곳이 벽인 경우
+        if board[nx][ny] == 2:
+            return []
+        else:
+            moved_sir.add(i)
+    # 밀릴 기사들의 좌표를 이동
+    for idx in moved_sir:
+        sirs[idx][0][0] += dx[d]
+        sirs[idx][0][1] += dy[d]
+
+
+    return moved_sir
+
+def get_damege(moved_sir, id):
+    if not moved_sir:
+        return
+    states = {}
+    # 기사 범위 좌표 구하기
+    for idx in moved_sir:
+        temp = []
+        states[idx] = get_xy(sirs[idx][0], temp)
+    
+    # 좌표를 돌면서 함정을 지나면 데미지 입기
+    for key, value in states.items():
+        cnt = 0
+        for x, y in value:
+            # 함정의 개수 세기
+            if board[x][y] == 1:
+                cnt += 1
+        # 남은 피 <= 입는 데미지: 데미지 없앰 & 체스판에서 없애기
+        # if sirs[key][1] - cnt <= 0:
+                
+        # 19번에서 에러가 난 이유 : 이미 피를 -시켰는데 죽어버린 경우까지 포함해버림
+        # damege를 따로 관리로 해결 (damege를 따로 관리하면서 원래의 피도 깎아줘야함)
+        if  cnt >= sirs[key][1]:
+            damege[key] = 0
+            del sirs[key]
+        else:
+            sirs[key][1] -= cnt
+            damege[key] += cnt
+
+# 로봇 해시로 저장
+# [로봇의 위치, h, w], k
+for idx, sir in enumerate(fighter):
+    r,c,h,w,k = sir[0]-1, sir[1]-1, sir[2], sir[3], sir[4]
+    sirs[idx+1].append([r, c, h, w])
+    sirs[idx+1].append(k)
+ #print(sirs)
+
+ # 0:위, 1:오른, 2:아래, 3:왼
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+damege = {i+1:0 for i in range(N)}  # 데미지를 저장
+
+for i, d in order:
+    moved_sir = set()    # 이동 한 기사
+
+    # 1. 기사 이동
+    moved_sir = move_sirs(i, d)
+
+    # 명령을 받은 기사는 데미지x
+    if moved_sir:
+        moved_sir.remove(i)
+
+    #2. 데미지 입기
+    get_damege(moved_sir, i)
+
+# 명령이 끝난 후 살아남은 기사의 데미지 합
+print(sum(damege.values()))
