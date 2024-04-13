14aa55b4ceb038a3f55872af54412ce669e3e6ac
김진기 <151550964+Jingik@users.noreply.github.com>
2024-04-13 19:23:58 +0900

    Title: 코드트리 빵, Time: 221ms, Memory: 26MB, Status: Passed - CodeTree

--- 240413/코드트리 빵/codetree-mon-bread.py
+++ 240413/코드트리 빵/codetree-mon-bread.py
@@ -1,102 +1,89 @@
 from collections import deque
-n,m = map(int,input().split())
-board=[]
-for _ in range(n):
-    board.append(list(map(int,input().split())))
-lock=[[0]*n for _ in range(n)]
 
-people=[(-1,-1) for _ in range(m+1)]
-conv=[(-1,-1)]
+N, M = map(int, input().split())
+arr = [[1] * (N+2)]
+for i in range(N):
+    arr.append([1] + list(map(int, input().split())) + [1])
+arr.append([1] *(N+2))
 
-visited=[[0]*n for _ in range(n)]
-step = [[0] * n for _ in range(n)]
+basecamp = set() # 중복 안됨
 
-# 편의점(도착지) 정보 위치
-for _ in range(m):
-    cy,cx = map(int,input().split())
-    conv.append((cy-1,cx-1))
+for j in range(1, N+1):
+    for i in range(1, N+1):
+        if arr[j][i] == 1:
+            basecamp.add((j, i))
+            arr[j][i] = 0
 
-def isAllPassed():
-    for i in range(1,m+1):
-        if people[i]!=conv[i]:
-            return False
-    return True
+store = {} # 편의점 위치
 
-d=[(-1,0),(0,-1),(0,1),(1,0)] # ↑, ←, →, ↓ 의 우선 순위
+for m in range(1, M+1):
+    j, i = map(int, input().split())
+    store[m] = (j, i)
 
-def bfs(sy,sx): # bfs 시작 좌표를 매개변수로 받아옴
-    # visited, step 값을 전부 초기화합니다.
-    for i in range(n):
-        for j in range(n):
-            visited[i][j] = 0
-            step[i][j] = 0
+def find(sj, si, dests): # 시작좌표에서 목적지 좌표들 set 중 최단거리 동일반경 리스트를 모두 찾는 것
+    dy, dx = [-1, 0, 0, 1], [0,-1, 1, 0]
     q = deque()
-    q.append((sy, sx))
-    visited[sy][sx] = 1
+    tlist = []
+    visited = [[False]*(N+2) for _ in range(N+2)]
+    q.append((sj, si))
+    visited[sj][si] = True
     while q:
-        y, x = q.popleft()
-        for dy, dx in d:
-            Y = y + dy
-            X = x + dx
-            if 0 <= Y < n and 0 <= X < n and not visited[Y][X] and lock[Y][X] == 0:
-                visited[Y][X] = 1
-                step[Y][X] = step[y][x] + 1
-                q.append((Y, X))
+        # 동일 범위(반경) 까지 처리하려면?
+        nq = deque()
+        # 뻗어가는건 new q 에 넣음
+        for cj, ci in q:
+            if (cj, ci) in dests: # 목적지 찾음
+                tlist.append((cj, ci))
+            else:
+                # 네방향, 미방문, 조건 : arr[][] == 0
+                for idx in range(4):
+                    nj, ni = cj + dy[idx], ci + dx[idx]
+                    if not visited[nj][ni] and arr[nj][ni] == 0:
+                        nq.append((nj, ni))
+                        visited[nj][ni] = True
+        # 목적지 찾앗으면 리턴해야함
+        if len(tlist) > 0:
+            tlist.sort()
+            return tlist[0]
+        q = nq
 
-def lock_board():
-    for i in range(1,m+1):
-        if people[i]==conv[i]:
-            py,px=people[i]
-            lock[py][px]=1
+def solve():
+    q = deque()
+    time = 1
+    arrived = [0] * (M+1) # 0이면 미도착, >0 이면 도착
 
+    while q or time == 1: #처음 또는 q에 데이터가 있는 동안 (이동할 사람이 있는 동안)
+        # 1. 모두 편의점 방향 최단거리 이동
+        nq = deque()
+        alist = []
+        for cj, ci, num in q:
+            if arrived[num] == 0: # 도착하지 않은 사람만
+                # 편의점 방향 최단거리 한칸 이동
+                # 편의점에서 시작, 현재위치 상하좌우
+                nj, ni = find(store[num][0], store[num][1], set(((cj-1, ci), (cj+1, ci), (cj, ci-1), (cj, ci+1))))
+                if (nj, ni) == (store[num]):
+                    arrived[num] = time
+                    alist.append((nj, ni)) # 통행 금지는 모두 이동 후 처리해야함
+                else:
+                    nq.append((nj, ni, num)) # 계속 이동
+        q = nq
+        # 2. 편의점 도착처리 -> arr[][] = 1 (이동불가 처리)
 
+        if len(alist) > 0:
+            for (aj, ai) in alist:
+                arr[aj][ai] = 1 # 이동불가 처리
 
-def enterBaseCamp(time):
-    global m,n,d,board,people
-    cy,cx=conv[time]
-    bfs(cy, cx)
-    dist = 1e9  # 최단거리값
-    by, bx = -1, -1
-    for i in range(n):
-        for j in range(n):
-            # 방문 가능한 베이스 캠프 중 거리가 가장 가까운 위치를 찾기
-            if visited[i][j] and board[i][j] == 1 and dist > step[i][j]:
-                dist = step[i][j]
-                by,bx = i, j
-    people[time]=(by,bx)
-    lock[by][bx]=1
-    # print(baseCamp[time],people[time])
+        # 3. 시간번호의 멤버가 베이스캠프로 순간이동
+        if time <= M:
+            # 시작좌표 -> 편의점
+            sj, si = store[time]
+            ej, ei = find(sj, si, basecamp) # 가장 가까운 베이스캠프 선택
+            basecamp.remove((ej, ei))
+            arr[ej][ei] = 1   # 이동 불가 표시
+            q.append((ej, ei, time)) # 뻗어나가야함. 베이스캠프에서 시작
 
+        time += 1
+    return max(arrived)
 
-
-
-def simulate():
-    for i in range(1,m+1):
-        # 아직 격자 밖에 있는 사람이거나 이미 편의점에 도착한 사람이라면 패스
-        if people[i] == (-1,-1) or people[i] == conv[i]:
-            continue
-        cy,cx=conv[i]
-        bfs(cy,cx)
-        py,px = people[i]
-        dist = 1e9
-        ty, tx = -1, -1  # tmp 좌표
-        for dy, dx in d:
-            PY = py + dy
-            PX = px + dx
-            if 0 <= PY < n and 0 <= PX < n and visited[PY][PX] and dist > step[PY][PX]:
-                dist = step[PY][PX]
-                ty, tx = PY, PX
-        people[i] = (ty, tx)
-    lock_board()
-    if time>m:
-        return
-    enterBaseCamp(time) 
-    # print('1턴')
-time=0
-while 1:
-    time+=1
-    simulate()
-    # 전부 이동이 끝났다면 종료
-    if isAllPassed():
-        break
-print(time)
+ans=solve()
+print(ans)
