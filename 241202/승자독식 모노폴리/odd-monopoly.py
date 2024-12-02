e94f878f7a010f4dedb80e67c83d7b44a932c4d5
김진기 <151550964+Jingik@users.noreply.github.com>
2024-12-02 20:26:46 +0900

    Title: 승자독식 모노폴리, Time: 222ms, Memory: 23MB, Status: Passed - CodeTree

--- 241202/승자독식 모노폴리/odd-monopoly.py
+++ 241202/승자독식 모노폴리/odd-monopoly.py
@@ -1,115 +1,118 @@
-# n* n 격자칸 모노폴리 게임
-# m개의 플레이어
+import sys
+#sys.stdin = open('승자독식 모노폴리.txt','r')
 
-# 턴 한 번에 플레이어 한 칸씩 이동 -> 초기 위치도 독점계약 (독점 계약은 k만큼의 턴 동안만 유효)
-# 플레이어는 아무도 맺지않은 상하좌우로 이동하고 없을 경우에는 4방향 중 본인이 독점계약한 땅으로 이동
-# 칸이 여러개일 대 상하조우 순으로 우선순위 준다 (플레이어가 보고 있는 방향은 직전에 이동한 방향)
-# 한 칸에 여러 플레이어가 있을 경우에는 가장 작은 번호를 가진 플레이어만 살아남는다
+n,m,k = map(int,input().split())
+dx=[0,-1,1,0,0]
+dy=[0,0,0,-1,1]
+maps=[list(map(int,input().split())) for _ in range(n)]
+player_dir = {}
+player_dirs = list(map(int,input().split()))
+player_four_dir = [ [] for _ in range(m+1)]
 
-# 여러 플레이어가 있는 경우에는 같은 턴에서 플레이어가 같은 곳을 갈 때 있는다
+contract_map = [ [(0,0) for _ in range(n)] for _ in range(n)]
+for ply in range(len(player_dirs)):
+    plys = player_dirs[ply]
+    player_dir[ply+1] = plys
 
-# 정답은 1번 플레이어가 살아남기 까지 걸리는 시간
-from collections import deque
+for peo in range(m) :
+    for _ in range(4) :
+        a,b,c,d = map(int,input().split())
+        player_four_dir[peo+1].append([a,b,c,d])
 
-# 방향 정의: 상, 하, 좌, 우
-DIRECTIONS = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+def debug() :
+    print('계약맵')
+    for k in contract_map:
+        print(*k)
+    print('일반맵')
+    for x in maps:
+        print(*x)
+    print('플레이어 보는방향')
+    print(player_dir)
 
-def is_valid(x, y, n):
-    """격자 범위 내에 있는지 확인"""
-    return 0 <= x < n and 0 <= y < n
 
-def update_map(Map, n):
-    """독점 계약 유효 턴 감소"""
+def in_range(x,y):
+    return 0<=x<n and 0<=y<n
+
+def player_contract():
+    #해당위치에 서잇는 플레이어들 계약 시키기.
+    for i in range(n) :
+        for j in range(n) :
+            if maps[i][j] >0 : #사람이라면
+                player_num = maps[i][j]
+                contract_map[i][j] = (player_num,k)
+    return
+
+def player_move():
+    clear_arr=[]
+    update_arr = []
+    for i in range(n) :
+        for j in range(n) :
+            if maps[i][j] > 0 : #사람이면
+                possible = False
+                clear_arr.append((i,j))
+                p_num = maps[i][j]
+                p_dir = player_dir[p_num]
+
+                speicial_dir = player_four_dir[p_num]  # 해당 플레이어에 대한 4가지 방향값
+                speicial_dir_2 = speicial_dir[p_dir - 1]
+                for nums in speicial_dir_2:
+                    nx,ny = i+dx[nums],j+dy[nums]
+                    if in_range(nx,ny) and contract_map[nx][ny] == (0,0) :
+                        update_arr.append((nx,ny,p_num))
+                        player_dir[p_num] = nums
+                        possible = True
+                        break
+                if possible == False:
+                # 여기는 방향안돼, 주변(0,0)없어.
+                # 인접 4방향 중 본인이 독점계약한 땅으로 이동.
+                    for nums in speicial_dir_2:
+                        nx,ny = i+dx[nums],j+dy[nums]
+                        if in_range(nx,ny):
+                            next_num,_ = contract_map[nx][ny]
+                            if next_num == p_num :
+                                update_arr.append((nx,ny,p_num))
+                                player_dir[p_num] = nums
+                                break
+                # 인접한 4방향중 내가 독점계약한 땅으로
+
+
+    update_arr.sort(key = lambda x:(-x[2]))
+    for xx,yy in clear_arr:
+        maps[xx][yy] = 0
+    for xxx,yyy,pp in update_arr:
+        maps[xxx][yyy]=pp
+    contract_reduce(clear_arr)
+    return
+def contract_reduce(clear_arr):
     for i in range(n):
         for j in range(n):
-            if Map[i][j] != 0:  # 독점 계약이 있는 칸
-                player, turns_left = Map[i][j]
-                if turns_left > 1:
-                    Map[i][j] = (player, turns_left - 1)
-                else:
-                    Map[i][j] = 0  # 독점 계약 종료
+            a, b = contract_map[i][j]
+            if  a != 0 :
+                if b == 1 :
+                    contract_map[i][j] = 0, 0
+                else :
+                    contract_map[i][j] = a,b-1
 
-def players_move(players, player_dir, player_priority, Map, n, k):
-    """모든 플레이어의 이동 처리"""
-    new_positions = {}  # 이동 결과를 저장하는 임시 구조
+    return
 
-    for pl, (x, y) in players.items():
-        if (x, y) == (-1, -1):  # 탈락한 플레이어는 무시
-            continue
+def check_one():
+    count =0
+    for i in range(n) :
+        for j in range(n) :
+            if maps[i][j] > 0 :
+                count+=1
+    return count
 
-        direction = player_dir[pl - 1]
-        moved = False
-
-        # 우선순위에 따라 이동
-        for pri_dir in player_priority[pl - 1][direction - 1]:
-            nx, ny = x + DIRECTIONS[pri_dir - 1][0], y + DIRECTIONS[pri_dir - 1][1]
-            if is_valid(nx, ny, n) and Map[nx][ny] == 0:  # 빈 칸
-                new_positions.setdefault((nx, ny), []).append(pl)
-                players[pl] = (nx, ny)  # 플레이어 위치 갱신
-                player_dir[pl - 1] = pri_dir  # 이동 방향 갱신
-                moved = True
-                break
-
-        # 이동하지 못하면 본인의 독점 계약된 칸으로 이동
-        if not moved:
-            for pri_dir in player_priority[pl - 1][direction - 1]:
-                nx, ny = x + DIRECTIONS[pri_dir - 1][0], y + DIRECTIONS[pri_dir - 1][1]
-                if is_valid(nx, ny, n) and Map[nx][ny] != 0 and Map[nx][ny][0] == pl:
-                    new_positions.setdefault((nx, ny), []).append(pl)
-                    players[pl] = (nx, ny)
-                    player_dir[pl - 1] = pri_dir
-                    break
-
-    # 충돌 처리
-    for (nx, ny), player_list in new_positions.items():
-        if len(player_list) > 1:  # 충돌 발생
-            survivor = min(player_list)  # 가장 작은 번호의 플레이어만 남음
-            players[survivor] = (nx, ny)
-            Map[nx][ny] = (survivor, k)  # 독점 계약 갱신
-            for defeated in player_list:
-                if defeated != survivor:
-                    players[defeated] = (-1, -1)  # 탈락 처리
-        else:
-            pl = player_list[0]
-            Map[nx][ny] = (pl, k)  # 독점 계약 갱신
-
-def monopoly_game(n, m, k, Map, players, player_dir, player_priority):
-    """메인 게임 루프"""
-    time = 0
-    while len([pl for pl, pos in players.items() if pos != (-1, -1)]) > 1:
-        time += 1
-        if time > 1000:  # 1000턴 초과 시 중단
-            return -1
-
-        players_move(players, player_dir, player_priority, Map, n, k)
-        update_map(Map, n)  # 턴 종료 후 독점 계약 갱신
-
-        if players[1] == (-1, -1):  # 1번 플레이어 탈락
-            return -1
-
-    return time
-
-# 입력 처리
-n, m, k = map(int, input().split())
-Map = [[0] * n for _ in range(n)]
-players = {}
-
-for i in range(n):
-    row = list(map(int, input().split()))
-    for j in range(n):
-        if row[j] != 0:
-            players[row[j]] = (i, j)
-            Map[i][j] = (row[j], k)
-
-player_dir = list(map(int, input().split()))
-
-player_priority = []
-for _ in range(m):
-    priorities = []
-    for _ in range(4):
-        priorities.append(list(map(int, input().split())))
-    player_priority.append(priorities)
-
-# 게임 실행 및 결과 출력
-result = monopoly_game(n, m, k, Map, players, player_dir, player_priority)
-print(result)
+ok = False
+#debug()
+for rounds in range(1,1001) :
+    player_contract()
+    player_move()
+    #debug()
+    #print('라운드:',rounds)
+    if check_one() == 1 :
+        print(rounds)
+        ok = True
+        break
+if ok == False:
+    print(-1)
