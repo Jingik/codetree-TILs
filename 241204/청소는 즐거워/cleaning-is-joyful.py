7f7f0235406c11da4e413720004dc666afa5ca2f
김진기 <151550964+Jingik@users.noreply.github.com>
2024-12-04 16:39:29 +0900

    Title: 청소는 즐거워, Time: 454ms, Memory: 23MB, Status: Passed - CodeTree

--- 241204/청소는 즐거워/cleaning-is-joyful.py
+++ 241204/청소는 즐거워/cleaning-is-joyful.py
@@ -1,110 +1,96 @@
-# 변수 선언 및 입력
-n = int(input())
-grid = [
-    list(map(int, input().split()))
-    for _ in range(n)
-]
-
-# 시작 위치와 방향, 
-# 해당 방향으로 이동할 횟수를 설정합니다.
-curr_x, curr_y = n // 2, n // 2
-move_dir, move_num = 0, 1
-
-ans = 0
-
+DIRECTION = [(0, -1), (1, 0), (0, 1), (-1, 0)]  # 방향: 왼쪽, 아래, 오른쪽, 위
 dust_ratio = [
-    [
-        [0,  0, 2, 0, 0],
+    [  # 왼쪽
+        [0, 0, 2, 0, 0],
         [0, 10, 7, 1, 0],
-        [5,  0, 0, 0, 0],
+        [5, 0, 0, 0, 0],
         [0, 10, 7, 1, 0],
-        [0,  0, 2, 0, 0],
+        [0, 0, 2, 0, 0],
     ],
-    [
-        [0,  0, 0,  0, 0],
-        [0,  1, 0,  1, 0],
-        [2,  7, 0,  7, 2],
+    [  # 아래쪽
+        [0, 0, 0, 0, 0],
+        [0, 1, 0, 1, 0],
+        [2, 7, 0, 7, 2],
         [0, 10, 0, 10, 0],
-        [0,  0, 5,  0, 0],
+        [0, 0, 5, 0, 0],
     ],
-    [
-        [0, 0, 2,  0, 0],
+    [  # 오른쪽
+        [0, 0, 2, 0, 0],
         [0, 1, 7, 10, 0],
-        [0, 0, 0,  0, 5],
+        [0, 0, 0, 0, 5],
         [0, 1, 7, 10, 0],
-        [0, 0, 2,  0, 0],
+        [0, 0, 2, 0, 0],
     ],
-    [
-        [0,  0, 5,  0, 0],
+    [  # 위쪽
+        [0, 0, 5, 0, 0],
         [0, 10, 0, 10, 0],
-        [2,  7, 0,  7, 2],
-        [0,  1, 0,  1, 0],
-        [0,  0, 0,  0, 0],
-    ]
+        [2, 7, 0, 7, 2],
+        [0, 1, 0, 1, 0],
+        [0, 0, 0, 0, 0],
+    ],
 ]
 
+def is_valid(x, y, n):
+    """좌표가 유효한지 확인."""
+    return 0 <= x < n and 0 <= y < n
 
-def in_range(x, y):
-    return 0 <= x and x < n and 0 <= y and y < n
-
-
-# (x, y) 위치에 dust 만큼의 먼지를 추가합니다.
-def add_dust(x, y, dust):
+def spread_dust(x, y, direction, grid, n):
+    """현재 위치에서 먼지를 퍼뜨림."""
     global ans
-    
-    # 격자 범위를 벗어난다면 답에 더해줍니다.
-    if not in_range(x, y):
-        ans += dust
-    # 격자 범위 안이라면, 해당 칸에 더해줍니다.
+    total_dust = grid[x][y]
+    grid[x][y] = 0
+    spread_sum = 0
+
+    for i in range(-2, 3):
+        for j in range(-2, 3):
+            nx = x + i
+            ny = y + j
+            spread_amount = (total_dust * dust_ratio[direction][i + 2][j + 2]) // 100
+            spread_sum += spread_amount
+
+            if is_valid(nx, ny, n):
+                grid[nx][ny] += spread_amount
+            else:
+                ans += spread_amount  # 격자 밖으로 나간 먼지 누적
+
+    # 남은 먼지 a% 처리
+    alpha_x = x + DIRECTION[direction][0]
+    alpha_y = y + DIRECTION[direction][1]
+    remaining_dust = total_dust - spread_sum
+    if is_valid(alpha_x, alpha_y, n):
+        grid[alpha_x][alpha_y] += remaining_dust
     else:
-        grid[x][y] += dust
+        ans += remaining_dust  # 격자 밖으로 나간 먼지 누적
 
+def spiral_clean(n, grid):
+    """중앙에서 나선형으로 이동하며 청소."""
+    global ans
+    x, y = n // 2, n // 2  # 시작 위치 (중앙)
+    steps = 1  # 이동 거리
+    direction_index = 0  # 방향 인덱스
 
-# 한 칸 움직이며 청소를 진행합니다.
-def move():
-    global curr_x, curr_y
-    
-    # 문제에서 원하는 진행 순서대로 
-    # 왼쪽 아래 오른쪽 위 방향이 되도록 정의합니다.
-    dxs, dys = [0, 1, 0, -1], [-1, 0, 1, 0]
-    
-    # curr 위치를 계산합니다.
-    curr_x, curr_y = curr_x + dxs[move_dir], curr_y + dys[move_dir]
-    
-    # 현재 위치를 기준으로 각 위치에 먼지를 더해줍니다.
-    added_dust = 0
-    for i in range(5):
-        for j in range(5):
-            dust = grid[curr_x][curr_y] * dust_ratio[move_dir][i][j] // 100
-            add_dust(curr_x + i - 2, curr_y + j - 2, dust)
+    while True:
+        for _ in range(2):  # 동일한 이동 거리로 두 방향 이동
+            for _ in range(steps):
+                dx, dy = DIRECTION[direction_index]
+                x += dx
+                y += dy
+
+                if not is_valid(x, y, n):
+                    return ans  # 나선형 종료
+
+                spread_dust(x, y, direction_index, grid, n)  # 먼지 퍼뜨리기
             
-            added_dust += dust
-    
-    # a% 자리에 먼지를 추가합니다.
-    add_dust(curr_x + dxs[move_dir], curr_y + dys[move_dir], 
-             grid[curr_x][curr_y] - added_dust)
-
-
-def end():
-    return not curr_x and not curr_y
-
-
-while not end():
-    # move_num 만큼 이동합니다.
-    for _ in range(move_num):
-        move()
+            direction_index = (direction_index + 1) % 4  # 방향 전환
         
-        # 이동하는 도중 (0, 0)으로 오게 되면,
-        # 움직이는 것을 종료합니다.
-        if end():
-            break
-    
-    # 방향을 바꿉니다.
-    move_dir = (move_dir + 1) % 4
-    # 만약 현재 방향이 왼쪽 혹은 오른쪽이 된 경우에는
-    # 특정 방향으로 움직여야 할 횟수를 1 증가시킵니다.
-    if move_dir == 0 or move_dir == 2:
-        move_num += 1
+        steps += 1  # 이동 거리 증가
 
-# 출력:
-print(ans)
+
+# 입력
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+# 초기화 및 실행
+ans = 0
+result = spiral_clean(n, grid)
+print(result)
